3f88e4f2e13daec26203592ac9d53dfc379891b1
Caitlin Swickard <caitlin6029@gmail.com>
2021-05-07 18:04:54 -0600

    Moving functions around in JS

--- script.js
+++ script.js
@@ -3,64 +3,36 @@
 var generateBtn = document.querySelector("#generate");
 
 // Selection of characters that can be used to create password
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var specialCharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-var numbers = '0123456789';
+let lowerCase = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ];
+let upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ];
+let numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ];
+let specialCharacters = new Array(" !#$%&'()*+,-./:;<=>?@[]^_`{|}~");
 // Empty variable for characters
-var passwordCharacters = "";
-
+var passwordCharacters = [];
 
 
 // Write password to the #password input
 const writePassword = () => {
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  // adding character selection to passwordCharacters variable
-  if (lowerCase) {
-    passwordCharacters = passwordCharacters.concat(lowerCase)
-    }
-    if (upperCase) {
-    passwordCharacters = passwordCharacters.concat(upperCase);
-    }
-    if (specialCharacters) {
-    passwordCharacters = passwordCharacters.concat(specialCharacters);
-    }
-    if (numbers) {
-    passwordCharacters = passwordCharacters.concat(numbers);
-    }
-    console.log(passwordCharacters);
-    return passwordCharacters;
-   
-    
-    // for (let i = 0; i < passwordCharacters; i++) {
-    //   [Math.floor(Math.random() * passwordLength)]
-    //   passwordCharacters.concat(writePassword);
-    //   console.log(writePassword);
-    // }
-  }
+}
  
-
-
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
+// Empty array for new generated password
+var newPassword = [];
+
 
 // Asking for the length of password
-let generatePassword = () => {
-  var newPassword = "";
-
+function generatePassword (passwordLength) {
   var passwordLength = prompt('How many characters would you like to include in the password?');
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Password must be between 8 and 128 characters");
-    return;
+    return generatePassword();
 
     // Asking for characters to use in password
   } else if (passwordLength >= 8 && passwordLength <= 128) {
@@ -68,6 +40,7 @@ let generatePassword = () => {
     var upperCase = confirm("Would you like to include uppercase letters?");
     var specialCharacters = confirm("Would you like to include special characters?");
     var numbers = confirm("Would you like to include numbers?");
+    // return passwordLength;
   } 
   // If user cancels out of all options this alert will generate
   if (
@@ -78,27 +51,47 @@ let generatePassword = () => {
       alert("You must select some characters!");
       return (generatePassword());
     }
-}  
 
-  
-
-// create for loop to choose password characters
-
-for (let i = 0; i < passwordCharacters; i++) {
-  [Math.floor(Math.random() * passwordLength)]
-  passwordCharacters.concat(writePassword);
-  console.log(writePassword);
+// adding character selection to passwordCharacters variable
+    if (lowerCase) {
+      passwordCharacters = passwordCharacters.concat(lowerCase);
+      newPassword += charAt(lowerCase);
+      }
+      if (upperCase) {
+      passwordCharacters = passwordCharacters.concat(upperCase);
+      newPassword += charAt(upperCase);
+      }
+      if (specialCharacters) {
+      passwordCharacters = passwordCharacters.concat(specialCharacters);
+      newPassword += charAt(specialCharacters);
+      }
+      if (numbers) {
+      passwordCharacters = passwordCharacters.concat(numbers);
+      newPassword += charAt(numbers);
+      }
+      console.log(passwordCharacters);
+      return passwordCharacters;
 }
+ 
 
-
-function getPassword (passwordCharacters) {
-  console.log(passwordCharacters);
-for (var i = 0; i <= passwordLength; i++) {
-  newPassword += passwordCharacters.forEach(Math.floor(Math.random() * Math.floor(value.length -1)));
-  return newPassword;
+  for (var i = 0; i < passwordLength; i++) {
+    newPassword.push(passwordCharacters.charAt(Math.floor(Math.random () * passwordLength)));
+    console.log(makePassword);
   }
 
-}
+
+
+
+
+// function getPassword (passwordCharacters) {
+//   console.log(passwordCharacters);
+for (var i = 0; i <= passwordLength; i++) {
+  newPassword += passwordCharacters.charAt(Math.floor(Math.random() * Math.floor(value.length -1)));
+  // return newPassword;
+  }
+
+
+
 
 // add password to display area
 document.getElementById("#password").value = newPassword;
